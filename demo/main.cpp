0f56dd9d1dcd3ffdc84988debc28c82f62eb98a4
dashasheveleva <photodoshfy@gmail.com>
2022-02-12 15:02:11 +0300

    tests for task 1

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,3 +1,5 @@
+// Copyright 2022 Shevelyova Darya photodoshfy@gmail.com
+
 #include <Stack_task_1.hpp>
 #include <Stack_task_2.hpp>
 
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,9 +1,63 @@
-// Copyright 2021 Your Name <your_email>
+// Copyright 2022 Shevelyova Darya photodoshfy@gmail.com
 
 #include <stdexcept>
 #include <gtest/gtest.h>
-#include <Stack_task_1.hpp>
+#include "Stack_task_1.hpp"
+#include "Stack_task_2.hpp"
 
-TEST(Example, EmptyTest) {
+TEST(Stack_1, PushTest) {
+  Stack_1<int> stack;
+  EXPECT_EQ(stack.stack_size(), 0);
+  stack.push(19);
+  EXPECT_EQ(stack.stack_size(), 1);
+  EXPECT_EQ(stack.head(), 19);
+  stack.push(20);
+  EXPECT_EQ(stack.stack_size(), 2);
+  EXPECT_EQ(stack.head(), 20);
+}
 
+TEST(Stack_1, PopTest) {
+  Stack_1<int> stack;
+  EXPECT_EQ(stack.stack_size(), 0);
+  EXPECT_ANY_THROW(stack.pop());
+  stack.push(19);
+  stack.push(20);
+  stack.pop();
+  EXPECT_EQ(stack.stack_size(), 1);
+  EXPECT_EQ(stack.head(), 19);
+  stack.pop();
+  EXPECT_EQ(stack.stack_size(), 0);
+  EXPECT_ANY_THROW(stack.head());
+}
+
+TEST(Stack_1, HeadTest) {
+  Stack_1<int> stack;
+  EXPECT_EQ(stack.stack_size(), 0);
+  EXPECT_ANY_THROW(stack.head());
+  stack.push(19);
+  EXPECT_EQ(stack.stack_size(), 1);
+  EXPECT_EQ(stack.head(), 19);
+  stack.push(20);
+  EXPECT_EQ(stack.stack_size(), 2);
+  EXPECT_EQ(stack.head(), 20);
+  stack.pop();
+  EXPECT_EQ(stack.stack_size(), 1);
+  EXPECT_EQ(stack.head(), 19);
+  stack.pop();
+  EXPECT_EQ(stack.stack_size(), 0);
+  EXPECT_ANY_THROW(stack.head());
+}
+
+TEST(Stack_1, MoveTest) {
+  EXPECT_TRUE(std::is_move_constructible<Stack_1<int>>::value);
+  EXPECT_TRUE(std::is_move_constructible<Stack_1<std::string>>::value);
+  EXPECT_TRUE(std::is_move_assignable<Stack_1<int>>::value);
+  EXPECT_TRUE(std::is_move_assignable<Stack_1<std::string>>::value);
+}
+
+TEST(Stack_1, CopyTest) {
+  EXPECT_FALSE(std::is_copy_constructible<Stack_1<int>>::value);
+  EXPECT_FALSE(std::is_copy_constructible<Stack_1<std::string>>::value);
+  EXPECT_FALSE(std::is_copy_assignable<Stack_1<int>>::value);
+  EXPECT_FALSE(std::is_copy_assignable<Stack_1<std::string>>::value);
 }
